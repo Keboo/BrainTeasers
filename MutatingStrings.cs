8c7827713e56ed1f60d26ddfc05469148003a11d
Kevin Bost <kitokeboo@gmail.com>
2023-02-06 23:49:52 -0800

    Solution

--- MutatingStrings.cs
+++ MutatingStrings.cs
@@ -15,6 +15,24 @@ public class MutatingStrings
 
     private static void CauseIdentityCrisis()
     {
-        //TODO: Implement method
+        // Rely on string interning: "Kelly" here will be the same object as "Kelly" in MutateTheUnnamed().
+        // CLR of .NET 4.5+ will intern string literals when assembly is loaded into AppDomain.
+        // In previous versions we should use string.Intern("Kelly") to be sure of valid reference.
+
+        //string whoAmI = string.Intern("Kelly");
+        const string target = "Kelly";
+        const string whoAmI = "Kevin";
+
+        // obtain pointer and iterate with replacing
+        unsafe
+        {
+            fixed (char* ptr = whoAmI)
+            {
+                for (int i = 0; i < whoAmI.Length; i++)
+                {
+                    ptr[i] = target[i];
+                }
+            }
+        }
     }
 }

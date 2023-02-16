17e7bd4ae0337fd44787978a93a2126493c2eeff
Kevin Bost <kitokeboo@gmail.com>
2023-02-15 20:32:04 -0800

    Solution

--- LongClassNames.cs
+++ LongClassNames.cs
@@ -1,5 +1,8 @@
 namespace BrainTeasers;
 
+//This is part of Solution 2
+//using Foo = Foo1234567890123456789012345678901234567890;
+
 public class LongClassNames
 {
     [Fact]
@@ -9,3 +12,14 @@ public class LongClassNames
         Assert.True(typeof(Foo).Name.Length > 42);
     }
 }
+//Solution 1: Requires C# 11 or later
+//File-scope types have a long compiler generated name that includes a hash of the type
+// https://learn.microsoft.com/dotnet/csharp/language-reference/keywords/file
+file class Foo { }
+
+
+//Solution 2
+//Declare a really long type name then use a using directive to alias it to Foo.
+//You also need to uncomment the using directive at the top of the file.
+//https://learn.microsoft.com/dotnet/csharp/language-reference/keywords/using-directive
+//public class Foo1234567890123456789012345678901234567890 { }

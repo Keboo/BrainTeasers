87dcf1564ac855cf5caa0b9b18e56eb4238aeeb4
Kevin Bost <kitokeboo@gmail.com>
2023-02-22 23:13:17 -0800

    Solution

--- GetValuesTests.cs
+++ GetValuesTests.cs
@@ -17,6 +17,29 @@ public static class Numbers
         _ = startingValue ?? throw new ArgumentNullException(nameof(startingValue));
         // Implement this method me to make the test pass
         //Do NOT edit anything above this line.
-        return Enumerable.Empty<int>();
+
+        //The invocation of an iterator method does not occur immediately.
+        //By using the yield statement, this method becomes an iterator method.
+        //https://learn.microsoft.com/dotnet/csharp/language-reference/statements/yield#execution-of-an-iterator
+        yield break;
+    }
+
+    //NB: A properly implemented iterator method, with pre-condition checks should be implemented in two parts.
+    // The outer method to do the pre-condition checks that occur immediately when the method is invoked.
+    // An inner (or second) method that is the actual iterator method for providing the values
+    public static IEnumerable<int> CorrectGetValues(int? startingValue)
+    {
+        //Pre-condition check
+        _ = startingValue ?? throw new ArgumentNullException(nameof(startingValue));
+
+        //Invoke iterator method
+        return CorrectGetValuesImplementation(startingValue.Value);
+
+        //Iterator method
+        static IEnumerable<int> CorrectGetValuesImplementation(int startingValue)
+        {
+            //TODO: Some implementation here
+            yield break;
+        }
     }
 }
